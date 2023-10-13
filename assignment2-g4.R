de2562b04b3e5741604bd7c4d78f71359a1ead9d
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2023-10-13 08:39:03 +0200

    first draft of task 1 done

--- assignment2-g4.R
+++ assignment2-g4.R
@@ -22,6 +22,8 @@ summary(data)
 
 
 # Task 1 ------------------------------------------------------------------
+
+
 # Prepare your data
 data$Abs_RET <- abs(data$RET)  # Compute absolute value of returns
 data$ln_SVI <- log(1 + data$SVI)  # Compute ln(1 + SVI)
@@ -29,15 +31,21 @@ data$ln_SVI <- log(1 + data$SVI)  # Compute ln(1 + SVI)
 # Model with homoscedastic standard errors
 homoscedastic_model <- lm(ln_SVI ~ Abs_RET, data = data)
 
-# Model with heteroscedasticity-corrected standard errors
-heteroscedasticity_corrected_errors <- coeftest(homoscedastic_model, vcov = vcovHC(homoscedastic_model, type = "HC3"))
+# Obtain heteroskedasticity-robust standard errors
+robust_se <- coeftest(homoscedastic_model, vcov = vcovHC(homoscedastic_model, type = "HC3"))[, 2]
 
-# Model with stock-clustered standard errors
-# Assuming 'stock' is the variable identifying different stocks
-stock_clustered_errors <- coeftest(homoscedastic_model, vcov = vcovCL(homoscedastic_model, cluster = ~ stock, data = data))
+# Obtain stock-clustered standard errors
+clustered_se <- coeftest(homoscedastic_model, vcov = vcovCL(homoscedastic_model, cluster = ~ ticker, data = data))[, 2]
 
-# Output results in a summary table
-stargazer(homoscedastic_model, heteroscedasticity_corrected_errors, stock_clustered_errors, type="text", se=list(NULL, vcovHC(homoscedastic_model, type = "HC3"), vcovCL(homoscedastic_model, cluster = ~ stock, data = data)))
+# Generate regression table
+stargazer(homoscedastic_model, homoscedastic_model, homoscedastic_model,
+          se = list(NULL, robust_se, clustered_se),
+          title = "Regression Table",
+          header = FALSE,
+          model.names = FALSE,
+          omit.stat = "all",
+          column.labels = c("Homoskedastic", "Heteroskedasticity-Corrected", "Stock-Clustered"),
+          type = "text")
 
 # Plot
 # Preparing predictions from the model
@@ -50,4 +58,4 @@ plot(data$RET, data$ln_SVI,
      main = "Plot with Predictions from the Model",
      pch = 20)
 abline(homoscedastic_model, col = "red")  # Adding regression line
-points(data$RET, data$predicted, col = "blue", pch = 20)  # Adding predicted points
+points(data$RET, data$predicted, col = "blue", pch = 20)  # Adding predicted points 
